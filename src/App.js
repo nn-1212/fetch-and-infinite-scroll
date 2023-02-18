1d7ea2928427712a2b49d1d9418bd9179c7194fa
allison <fidomoon@hotmail.com>
2023-02-18 20:20:46 +0800

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [coinData, setCoinData] = useState([])
+
+  const fetchData = ()=> {fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false")
+  .then((resp)=> resp.json())
+  .then((data)=>{
+    setCoinData(data)
+  })
+  }
+  
+  useEffect(fetchData,[])
+
+
   return (
     <div className="App">
+        <h1>Gallery</h1>
+        {coinData.map((item)=>{
+          return <div>{item.id}
+          <img src={item.image} alt=""/>
+          <p>{item.current_price}</p>
+          
+          </div>
+        })}
+           
+          
       
     </div>
   );
